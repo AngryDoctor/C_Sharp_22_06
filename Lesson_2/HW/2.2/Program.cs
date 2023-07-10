89cde17ad13db06447143da71dd4bb258ae4ac57
Anton <tagil.anton@yandex.ru>
2023-07-10 20:45:55 +0300

    Задание 2

---
+++ Lesson_2/HW/2.2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая выводит случайное трехкратное число и
+// удаляет вторую цифру этого числа
+
+Console.Clear();
+System.Console.WriteLine("");
+int num = new Random().Next(100, 1000);
+
+System.Console.WriteLine($"Второй цифрой числа {num} является {num / 10 % 10} ");
+
+System.Console.WriteLine();
+
+
