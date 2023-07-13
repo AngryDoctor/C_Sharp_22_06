9f46eab5c214ea24fc89458dccdc5f959a5ac9f0
Anton <tagil.anton@yandex.ru>
2023-07-13 19:41:20 +0300

    2

---
+++ Lesson_3/3.1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 1. Напишите программу, которая по заданному номеру четверти,
+// показывет диапазон возможных координат точек в этой четверти
+// (х и у)
+
+System.Console.WriteLine("Enter number of quarters: ");
+int quarter = int.Parse(Console.ReadLine()!);
+
+void Quarters(int  quarter)
+{
+     if (quarter > 4 || quarter < 1) System.Console.WriteLine("Erroe");
+    else if (quarter == 1) System.Console.WriteLine("x > 0 && y > 0");
+    else if (quarter == 2) System.Console.WriteLine("x < 0 && y > 0");
+    else if (quarter == 3) System.Console.WriteLine("x < 0 && y < 0");
+    else if (quarter == 4) System.Console.WriteLine("x > 0 && y < 0");
+}
+
+Quarters(quarter);
