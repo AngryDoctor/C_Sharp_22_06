6d6a240991b23c613235368c8f46e98891b28472
Anton <tagil.anton@yandex.ru>
2023-07-13 21:33:09 +0300

    Задача 4

---
+++ Lesson_3/3.3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 3. Напишите программу, которая принимает на вход число (N) и
+// выдает таблицу квадратов числе от 1 до N.
+
+Console.WriteLine("Введите число");
+double A1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число произведений в степень");
+int N = int.Parse(Console.ReadLine()!);
+double result = A1;
+int b = 1;
+
+if (N == 0)
+{
+    Console.WriteLine("значение равно 1");
+}
+else
+    do
+    {
+        result = result * A1;
+        b = b + 1;
+        Console.WriteLine("Значения = " + result);
+    } while (b <= N);
